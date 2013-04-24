f5a5f557bc2813bdf34d6ead5d70288c7fa0be16
Daniel Bradley <daniel@danielbradley.net>
2013-04-24 15:07:15 +0100

    Move API controllers

--- MvcApp/Controllers/API/TestController.cs
+++ MvcApp/Controllers/API/TestController.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using MvcApp.Models;
 
-namespace MvcApp.Controllers
+namespace MvcApp.Controllers.API
 {
     public class TestController : ApiController
     {
