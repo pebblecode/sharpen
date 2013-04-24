717fe4545da66b226d80dc841f713d0a06566769
Daniel Bradley <daniel@danielbradley.net>
2013-04-24 12:23:11 +0100

    Add data member and data contract attributes

--- MvcApp/Models/AnswerDto.cs
+++ MvcApp/Models/AnswerDto.cs
@@ -1,23 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
 namespace MvcApp.Models
 {
+    [DataContract]
     public class AnswerDto
     {
         [Required]
+        [DataMember(IsRequired = true)]
         [JsonProperty(PropertyName = "question")]
         public string Question { get; set; }
 
         [Required]
+        [DataMember(IsRequired = true)]
         [JsonProperty(PropertyName = "answer")]
         public string Answer { get; set; }
 
         [Required]
+        [DataMember(IsRequired = true)]
         [JsonProperty(PropertyName = "markingCriteria")]
         public string MarkingCriteria { get; set; }
 
         [Required]
+        [DataMember(IsRequired = true)]
         [Range(0, 999)]
         [JsonProperty(PropertyName = "points")]
         public int Points { get; set; }
--- MvcApp/Models/AnswersDto.cs
+++ MvcApp/Models/AnswersDto.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
 namespace MvcApp.Models
 {
+    [DataContract]
     public class AnswersDto
     {
         [JsonProperty(PropertyName = "id")]
         public Guid Id { get; set; }
 
         [Required]
+        [DataMember(IsRequired = true)]
         [JsonProperty(PropertyName = "testId")]
         public Guid TestId { get; set; }
 
         [Required]
+        [DataMember(IsRequired = true)]
         [JsonProperty(PropertyName = "userId")]
         public string UserId { get; set; }
 
+        [Required]
+        [DataMember(IsRequired = true)]
         [JsonProperty(PropertyName = "answers")]
         public IEnumerable<AnswerDto> Answers { get; set; }
     }
