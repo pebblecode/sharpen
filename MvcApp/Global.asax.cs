8372da5ad121845f7b57581e6fa18cc176e0b894
Daniel Bradley <daniel@danielbradley.net>
2013-04-24 11:26:37 +0100

    Allow requesting json via the command line
    Usage: ?json=true

--- MvcApp/Global.asax.cs
+++ MvcApp/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -25,6 +26,8 @@ namespace MvcApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("json", "true", "application/json"));
         }
     }
 }
