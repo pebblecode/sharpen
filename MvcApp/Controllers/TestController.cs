5ae125818829a697a0d5a571ed2eb6338ee7c809
Daniel Bradley <daniel@danielbradley.net>
2013-04-24 11:16:16 +0100

    Create test API controller and models

---
+++ MvcApp/Controllers/TestController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MvcApp.Models;
+
+namespace MvcApp.Controllers
+{
+    public class TestController : ApiController
+    {
+        private static readonly ConcurrentDictionary<Guid, TestDto> Tests = new ConcurrentDictionary<Guid, TestDto>();
+
+        // GET api/test
+        public IEnumerable<TestDto> Get()
+        {
+            return Tests.Values;
+        }
+
+        // GET api/test/5
+        public TestDto Get(Guid id)
+        {
+            TestDto test;
+            if (Tests.TryGetValue(id, out test))
+            {
+                return test;
+            }
+
+            throw new HttpResponseException(HttpStatusCode.NotFound);
+        }
+
+        // POST api/test
+        public TestDto Post(TestDto test)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var id = Guid.NewGuid();
+            test.Id = id;
+            if (!Tests.TryAdd(id, test))
+            {
+                throw new HttpResponseException(HttpStatusCode.Conflict);
+            }
+
+            return test;
+        }
+    }
+}
---
+++ MvcApp/Models/QuestionDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace MvcApp.Models
+{
+    public class QuestionDto
+    {
+        [Required]
+        [JsonProperty(PropertyName = "question")]
+        public string Question { get; set; }
+
+        [Required]
+        [JsonProperty(PropertyName = "markingCriteria")]
+        public string MarkingCriteria { get; set; }
+
+        [Required]
+        [Range(0, 999)]
+        public int Points { get; set; }
+    }
+}
